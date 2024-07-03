850c65ac53f5dd081c89fb7e6bfa0a7f06489700
LBarragan87 <80732243+LBarragan87@users.noreply.github.com>
2024-07-02 21:25:21 -0600

    Update CenterText.py

--- Utilities/CenterText.py
+++ Utilities/CenterText.py
@@ -29,13 +29,40 @@ from RevitServices.Transactions import TransactionManager
 #Get Document
 doc = DocumentManager.Instance.CurrentDBDocument
 
+#Create Functions
+def ElementsToList(Input):
+    newList=[]
+    try:
+        TextListCount=List.Count(Input)
+        newList=Input
+    except:
+        newList.append(Input)
+    return newList
+    
+def MtoFt(M):
+    return M/12/0.0254
+    
+def PointToXYZ(ProtogeometryPoint):
+    XFromInsertionPoint=MtoFt(Autodesk.DesignScript.Geometry.Point.get_X(ProtogeometryPoint))
+    YFromInsertionPoint=MtoFt(Autodesk.DesignScript.Geometry.Point.get_Y(ProtogeometryPoint))
+    ZFromInsertionPoint=MtoFt(Autodesk.DesignScript.Geometry.Point.get_Z(ProtogeometryPoint))
+    return XYZ(XFromInsertionPoint,YFromInsertionPoint,ZFromInsertionPoint)
 #Inputs
-texto = UnwrapElement(IN[0])
+TextList = UnwrapElement(IN[0]) #Select Elements to move
+InsertionPoint = UnwrapElement(IN[1]) #Specify new coordinates of Insertion Point (Coordinates on METERS)
 
-# Incluya el código debajo de esta línea
+#Input List Conversion
+convertedTextList=ElementsToList(TextList)
+
+# Start Code
 TransactionManager.Instance.EnsureInTransaction(doc)
-newCoord= XYZ(0,0,0)
-X=texto.set_Coord(newCoord)
+
+newCoord= PointToXYZ(InsertionPoint)
+
+for textNote in convertedTextList:
+    X=textNote.set_Coord(newCoord)
+
 TransactionManager.Instance.TransactionTaskDone()
-# Asigne la salida a la variable OUT.
-OUT = texto
+
+# Output
+OUT = convertedTextList
