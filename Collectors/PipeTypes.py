55c9f77817694eaee8e9c104cd0e754eb7c77f64
LBarragan87 <80732243+LBarragan87@users.noreply.github.com>
2024-07-08 09:06:24 -0600

    Create PipeTypes.py

---
+++ Collectors/PipeTypes.py
@@ -0,0 +1,36 @@
+import System
+import clr
+
+#import DSCoreNodes
+clr.AddReference('DSCoreNodes')
+from DSCore import *
+
+#Import ProtoGeometry
+clr.AddReference('ProtoGeometry')
+from Autodesk.DesignScript.Geometry import *
+
+# Import RevitNodes
+clr.AddReference("RevitNodes")
+from Revit import *
+
+# Import Revit elements
+from Revit.Elements import *
+
+# Import RevitAPI
+clr.AddReference("RevitAPI")
+import Autodesk
+from Autodesk.Revit.DB import *
+from Autodesk.Revit.DB.Plumbing import *
+# Import DocumentManager
+clr.AddReference("RevitServices")
+from RevitServices.Persistence import DocumentManager
+from RevitServices.Transactions import TransactionManager
+
+#Get Document
+doc = DocumentManager.Instance.CurrentDBDocument
+######################################################
+# Start Code
+PipeTypes = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_PipingSystem).WhereElementIsElementType().ToElements()
+######################################################
+# Output
+OUT = PipeTypes
