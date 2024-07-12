a9b329195b73504aafe308daad77a47267ba0e6d
LBarragan87 <80732243+LBarragan87@users.noreply.github.com>
2024-07-12 11:10:06 -0600

    Create FloorTypes.py

---
+++ Collectors/FloorTypes.py
@@ -0,0 +1,35 @@
+import System
+import clr
+
+#import DSCoreNodes
+clr.AddReference('DSCoreNodes')
+from DSCore import *
+
+#Import ProtoGeometry
+clr.AddReference('ProtoGeometry')
+from Autodesk.DesignScript.Geometry import *
+
+# Import RevitNodes
+clr.AddReference("RevitNodes")
+from Revit import *
+
+# Import Revit elements
+from Revit.Elements import *
+
+# Import RevitAPI
+clr.AddReference("RevitAPI")
+import Autodesk
+from Autodesk.Revit.DB import *
+# Import DocumentManager
+clr.AddReference("RevitServices")
+from RevitServices.Persistence import DocumentManager
+from RevitServices.Transactions import TransactionManager
+
+#Get Document
+doc = DocumentManager.Instance.CurrentDBDocument
+######################################################
+# Start Code
+FloorTypes = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Floors).WhereElementIsElementType().ToElements()
+######################################################
+# Output
+OUT = FloorTypes
